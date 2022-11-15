1d8646129afd50d9e76dff65530acf101ecc6846
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2022-11-15 14:06:11 -0600

    played with repeat
    fire is fun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,13 +31,17 @@ export class AppComponent implements OnInit {
       next: (data) => {
         console.log("data", data);
 
-        this.quizzes = data.map((x: any) => ({
-          quizName: x.name,
-          quizQuestions: x.questions.map((y: any) => ({
-            questionText: y.name
-          })),
-          markedForDelete: false
-        }));
+        this.quizzes = [
+          ...this.quizzes,
+          ...data.map((x: any) => ({
+            quizName: x.name,
+            quizQuestions: x.questions.map((y: any) => ({
+              questionText: y.name
+            })),
+            markedForDelete: false
+          }))
+        ];
+        
         console.log(this.quizzes);
       },
       error: (err) => {
