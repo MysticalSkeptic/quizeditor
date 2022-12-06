22f00b3b204549d641ac83605e97253b14e23fbe
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2022-12-06 13:21:42 -0600

    quick commit before changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
+import {
+  trigger,
+  transition,
+  animate,
+  keyframes,
+  style
+} from '@angular/animations';
 
 interface QuizDisplay {
   quizName: string;
@@ -16,7 +23,29 @@ interface QuestionDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('300ms', keyframes([
+          style({ marginLeft: '-30px', offset: 0.0 }),
+          style({ marginLeft: '-20px', offset: 0.25 }),
+          style({ marginLeft: '-10px', offset: 0.5 }),
+          style({ marginLeft: '-5px', offset: 0.75 }),
+          style({ marginLeft: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   
@@ -89,6 +118,7 @@ export class AppComponent implements OnInit {
 
   selectQuiz = (quizToSelect: QuizDisplay) => {
     this.selectedQuiz = quizToSelect;
+    this.detailsAnimationState = 'finalPosition';
   };
 
   addNewQuestion = () => {
@@ -200,6 +230,11 @@ export class AppComponent implements OnInit {
     !q.newlyAdded
     && !q.markedForDelete
     && this.generateNaiveChecksum(q) != q.naiveChecksum;
-  
 
+    detailsAnimationState = "leftPosition";
+
+    detailsAnimationDone = () => {
+      this.detailsAnimationState = "leftPosition";
+    }
 }
+
